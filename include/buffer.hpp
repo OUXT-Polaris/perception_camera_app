0f04799b25345f52269fc24ec5e12de947978992
Masaya Kataoka <ms.kataoka@gmail.com>
2022-03-10 17:56:29 +0000

    add emplace function

--- include/buffer.hpp
+++ include/buffer.hpp
@@ -16,12 +16,15 @@ template <typename T> bool operator<(const Data<T> &t1, const Data<T> &t2) {
   return t1.stamp < t2.stamp;
 }
 
-class Buffer : public std::queue<Data<cv::Mat>> {
+class Buffer {
 public:
   explicit Buffer(const std::chrono::milliseconds &duration);
   const std::chrono::milliseconds duration;
+  void emplace(const cv::Mat &image);
+  Data<cv::Mat> front();
 
 private:
   void popOld();
+  std::queue<Data<cv::Mat>> queue_;
 };
 } // namespace perception_camera_app
--- src/buffer.cpp
+++ src/buffer.cpp
@@ -4,12 +4,33 @@ namespace perception_camera_app {
 Buffer::Buffer(const std::chrono::milliseconds &duration)
     : duration(duration) {}
 
+void Buffer::emplace(const cv::Mat &image) {
+  queue_.emplace(image, std::chrono::system_clock::now());
+  popOld();
+}
+
+Data<cv::Mat> Buffer::front() {
+  popOld();
+  return queue_.front();
+}
+
 void Buffer::popOld() {
-  std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-  if (now - front().stamp > duration) {
-    pop();
-  } else if (now < front().stamp) {
-    pop();
+  auto check_stamp = [this]() {
+    if (queue_.size() == 0) {
+      return false;
+    }
+    std::chrono::system_clock::time_point now =
+        std::chrono::system_clock::now();
+    if (now - queue_.front().stamp > duration) {
+      queue_.pop();
+      return true;
+    } else if (now < queue_.front().stamp) {
+      queue_.pop();
+      return true;
+    }
+    return false;
+  };
+  while (check_stamp()) {
   }
 }
 } // namespace perception_camera_app
