21a3acdcc95e77cba24a2ad75cfd70e6d06bc436
Masaya Kataoka <ms.kataoka@gmail.com>
2022-03-13 11:07:15 +0000

    add conversion function

--- include/conversion.hpp
+++ include/conversion.hpp
@@ -5,29 +5,8 @@
 #include <perception_camera_app.pb.h>
 
 namespace perception_camera_app {
-cv::Mat convert(const perception_camera_app::Image &image) {
-  if (image.format() == perception_camera_app::ImageFormat::CV8U) {
-    std::vector<char> bytes(image.data().begin(), image.data().end());
-    return cv::Mat(image.height(), image.width(), CV_8U, &bytes[0]);
-  }
-  if (image.format() == perception_camera_app::ImageFormat::CV8UC1) {
-    std::vector<char> bytes(image.data().begin(), image.data().end());
-    return cv::Mat(image.height(), image.width(), CV_8UC1, &bytes[0]);
-  }
-  if (image.format() == perception_camera_app::ImageFormat::CV8UC2) {
-    std::vector<char> bytes(image.data().begin(), image.data().end());
-    return cv::Mat(image.height(), image.width(), CV_8UC2, &bytes[0]);
-  }
-  if (image.format() == perception_camera_app::ImageFormat::CV8UC3) {
-    std::vector<char> bytes(image.data().begin(), image.data().end());
-    return cv::Mat(image.height(), image.width(), CV_8UC3, &bytes[0]);
-  }
-  if (image.format() == perception_camera_app::ImageFormat::CV8UC4) {
-    std::vector<char> bytes(image.data().begin(), image.data().end());
-    return cv::Mat(image.height(), image.width(), CV_8UC4, &bytes[0]);
-  }
-  throw std::runtime_error("unsupported image format!!");
-}
+cv::Mat convert(const perception_camera_app::Image &image);
+perception_camera_app::Image convert(const cv::Mat &image);
 } // namespace perception_camera_app
 
 #endif // _PERCEPTION_CAMERA_APP__CONVERSION_HPP_
--- src/conversion.cpp
+++ src/conversion.cpp
@@ -1,1 +1,76 @@
 #include <conversion.hpp>
+
+namespace perception_camera_app {
+cv::Mat convert(const perception_camera_app::Image &image) {
+  if (image.format() == perception_camera_app::ImageFormat::CV8U) {
+    std::vector<char> bytes(image.data().begin(), image.data().end());
+    return cv::Mat(image.height(), image.width(), CV_8U, &bytes[0]);
+  }
+  if (image.format() == perception_camera_app::ImageFormat::CV8UC1) {
+    std::vector<char> bytes(image.data().begin(), image.data().end());
+    return cv::Mat(image.height(), image.width(), CV_8UC1, &bytes[0]);
+  }
+  if (image.format() == perception_camera_app::ImageFormat::CV8UC2) {
+    std::vector<char> bytes(image.data().begin(), image.data().end());
+    return cv::Mat(image.height(), image.width(), CV_8UC2, &bytes[0]);
+  }
+  if (image.format() == perception_camera_app::ImageFormat::CV8UC3) {
+    std::vector<char> bytes(image.data().begin(), image.data().end());
+    return cv::Mat(image.height(), image.width(), CV_8UC3, &bytes[0]);
+  }
+  if (image.format() == perception_camera_app::ImageFormat::CV8UC4) {
+    std::vector<char> bytes(image.data().begin(), image.data().end());
+    return cv::Mat(image.height(), image.width(), CV_8UC4, &bytes[0]);
+  }
+  throw std::runtime_error("unsupported image format!!");
+}
+
+perception_camera_app::Image convert(const cv::Mat &image) {
+  if (image.type() == CV_8U) {
+    perception_camera_app::Image proto;
+    proto.set_height(image.size().height);
+    proto.set_width(image.size().width);
+    proto.set_format(perception_camera_app::ImageFormat::CV8U);
+    int size = image.size().height * image.size().width;
+    proto.set_data(reinterpret_cast<char const *>(image.data), size);
+    return proto;
+  }
+  if (image.type() == CV_8UC1) {
+    perception_camera_app::Image proto;
+    proto.set_height(image.size().height);
+    proto.set_width(image.size().width);
+    proto.set_format(perception_camera_app::ImageFormat::CV8UC1);
+    int size = image.size().height * image.size().width;
+    proto.set_data(reinterpret_cast<char const *>(image.data), size);
+    return proto;
+  }
+  if (image.type() == CV_8UC2) {
+    perception_camera_app::Image proto;
+    proto.set_height(image.size().height);
+    proto.set_width(image.size().width);
+    proto.set_format(perception_camera_app::ImageFormat::CV8UC2);
+    int size = image.size().height * image.size().width * 2;
+    proto.set_data(reinterpret_cast<char const *>(image.data), size);
+    return proto;
+  }
+  if (image.type() == CV_8UC3) {
+    perception_camera_app::Image proto;
+    proto.set_height(image.size().height);
+    proto.set_width(image.size().width);
+    proto.set_format(perception_camera_app::ImageFormat::CV8UC3);
+    int size = image.size().height * image.size().width * 3;
+    proto.set_data(reinterpret_cast<char const *>(image.data), size);
+    return proto;
+  }
+  if (image.type() == CV_8UC4) {
+    perception_camera_app::Image proto;
+    proto.set_height(image.size().height);
+    proto.set_width(image.size().width);
+    proto.set_format(perception_camera_app::ImageFormat::CV8UC4);
+    int size = image.size().height * image.size().width * 4;
+    proto.set_data(reinterpret_cast<char const *>(image.data), size);
+    return proto;
+  }
+  throw std::runtime_error("unsupported image format!!");
+}
+} // namespace perception_camera_app
