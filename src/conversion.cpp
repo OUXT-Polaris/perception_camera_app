ed9bf631183161e3535112e1d7687bd510377141
MasayaKataoka <ms.kataoka@gmail.com>
2022-04-03 23:32:04 +0900

    use char
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- src/conversion.cpp
+++ src/conversion.cpp
@@ -3,23 +3,23 @@
 namespace perception_camera_app {
 cv::Mat convert(const perception_camera_app::Image &image) {
   if (image.format() == perception_camera_app::ImageFormat::CV8U) {
-    std::vector<uint8_t> bytes(image.data().begin(), image.data().end());
+    std::vector<char> bytes(image.data().begin(), image.data().end());
     return cv::Mat(image.height(), image.width(), CV_8U, &bytes[0]);
   }
   if (image.format() == perception_camera_app::ImageFormat::CV8UC1) {
-    std::vector<uint8_t> bytes(image.data().begin(), image.data().end());
+    std::vector<char> bytes(image.data().begin(), image.data().end());
     return cv::Mat(image.height(), image.width(), CV_8UC1, &bytes[0]);
   }
   if (image.format() == perception_camera_app::ImageFormat::CV8UC2) {
-    std::vector<uint8_t> bytes(image.data().begin(), image.data().end());
+    std::vector<char> bytes(image.data().begin(), image.data().end());
     return cv::Mat(image.height(), image.width(), CV_8UC2, &bytes[0]);
   }
   if (image.format() == perception_camera_app::ImageFormat::CV8UC3) {
-    std::vector<uint8_t> bytes(image.data().begin(), image.data().end());
+    std::vector<char> bytes(image.data().begin(), image.data().end());
     return cv::Mat(image.height(), image.width(), CV_8UC3, &bytes[0]);
   }
   if (image.format() == perception_camera_app::ImageFormat::CV8UC4) {
-    std::vector<uint8_t> bytes(image.data().begin(), image.data().end());
+    std::vector<char> bytes(image.data().begin(), image.data().end());
     return cv::Mat(image.height(), image.width(), CV_8UC4, &bytes[0]);
   }
   throw std::runtime_error("unsupported image format!!");
@@ -32,7 +32,7 @@ perception_camera_app::Image convert(const cv::Mat &image) {
     proto.set_width(image.size().width);
     proto.set_format(perception_camera_app::ImageFormat::CV8U);
     int size = image.size().height * image.size().width;
-    proto.set_data(reinterpret_cast<uint8_t const *>(image.data), size);
+    proto.set_data(reinterpret_cast<char const *>(image.data), size);
     return proto;
   }
   if (image.type() == CV_8UC1) {
@@ -41,7 +41,7 @@ perception_camera_app::Image convert(const cv::Mat &image) {
     proto.set_width(image.size().width);
     proto.set_format(perception_camera_app::ImageFormat::CV8UC1);
     int size = image.size().height * image.size().width;
-    proto.set_data(reinterpret_cast<uint8_t const *>(image.data), size);
+    proto.set_data(reinterpret_cast<char const *>(image.data), size);
     return proto;
   }
   if (image.type() == CV_8UC2) {
@@ -50,7 +50,7 @@ perception_camera_app::Image convert(const cv::Mat &image) {
     proto.set_width(image.size().width);
     proto.set_format(perception_camera_app::ImageFormat::CV8UC2);
     int size = image.size().height * image.size().width * 2;
-    proto.set_data(reinterpret_cast<uint8_t const *>(image.data), size);
+    proto.set_data(reinterpret_cast<char const *>(image.data), size);
     return proto;
   }
   if (image.type() == CV_8UC3) {
@@ -59,7 +59,7 @@ perception_camera_app::Image convert(const cv::Mat &image) {
     proto.set_width(image.size().width);
     proto.set_format(perception_camera_app::ImageFormat::CV8UC3);
     int size = image.size().height * image.size().width * 3;
-    proto.set_data(reinterpret_cast<uint8_t const *>(image.data), size);
+    proto.set_data(reinterpret_cast<char const *>(image.data), size);
     return proto;
   }
   if (image.type() == CV_8UC4) {
@@ -68,7 +68,7 @@ perception_camera_app::Image convert(const cv::Mat &image) {
     proto.set_width(image.size().width);
     proto.set_format(perception_camera_app::ImageFormat::CV8UC4);
     int size = image.size().height * image.size().width * 4;
-    proto.set_data(reinterpret_cast<uint8_t const *>(image.data), size);
+    proto.set_data(reinterpret_cast<char const *>(image.data), size);
     return proto;
   }
   throw std::runtime_error("unsupported image format!!");
